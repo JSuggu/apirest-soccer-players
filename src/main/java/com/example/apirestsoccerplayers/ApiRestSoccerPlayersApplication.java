90a89bed0365f2f0e4e54ea5f091f14ed1552af6
Azame Franco <azamefranco@gmail.com>
2023-09-02 14:56:39 -0300

    project started

---
+++ src/main/java/com/example/apirestsoccerplayers/ApiRestSoccerPlayersApplication.java
@@ -0,0 +1,13 @@
+package com.example.apirestsoccerplayers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSoccerPlayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSoccerPlayersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apirestsoccerplayers/ApiRestSoccerPlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirestsoccerplayers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSoccerPlayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
